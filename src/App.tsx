4689cd772a90a5d4d5bf9afc382a5eb522be6934
Kirill Kubryakov <kubryakov@gmail.com>
2019-09-13 21:38:30 +0300

    Add fake categories

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,33 @@
 import * as React from "react";
-import { groupBy } from "lodash";
+import { groupBy, keyBy } from "lodash";
 import styled from "styled-components";
 
 import GlobalStyle from "./GlobalStyle";
-import { fetchData, Paragraph } from "./api";
+import { fetchData, Paragraph, Category } from "./api";
 
 const Wrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
 `;
 
+const TableRow = styled.tr<{ color: string }>`
+  background: ${p => p.color};
+`;
+
+const ColumnWrapper = styled.div`
+  margin-right: 20px;
+`;
+
 interface State {
   paragraphs: Paragraph[];
+  categories: Category[];
 }
 
 const useData = (): State => {
-  const [data, setData] = React.useState<State>({ paragraphs: [] });
+  const [data, setData] = React.useState<State>({
+    paragraphs: [],
+    categories: []
+  });
   React.useEffect(() => {
     fetchData().then(data => setData(data));
   }, []);
@@ -24,29 +36,46 @@ const useData = (): State => {
 };
 
 const App = () => {
-  const { paragraphs } = useData();
+  const { paragraphs, categories } = useData();
 
   const byChapters = groupBy(paragraphs, "chapter");
+
+  const categoriesBySlug = keyBy(categories, "slug");
 
   return (
     <div>
       <GlobalStyle />
       <h1>Оглавление</h1>
       <Wrapper>
+        <ColumnWrapper>
+          <h2>Категории</h2>
+          <table>
+            <tbody>
+              {categories.map(category => (
+                <TableRow key={category.slug} color={category.color}>
+                  <td>{category.name}</td>
+                </TableRow>
+              ))}
+            </tbody>
+          </table>
+        </ColumnWrapper>
         {Object.entries(byChapters).map(([chapter, chapterParagraphs]) => (
-          <div key={chapter}>
+          <ColumnWrapper key={chapter}>
             <h2>Глава {chapter}</h2>
             <table>
               <tbody>
                 {chapterParagraphs.map(paragraph => (
-                  <tr key={paragraph.id}>
+                  <TableRow
+                    key={paragraph.id}
+                    color={categoriesBySlug[paragraph.categorySlug].color}
+                  >
                     <td>{paragraph.paragraph}</td>
                     <td>{paragraph.description}</td>
-                  </tr>
+                  </TableRow>
                 ))}
               </tbody>
             </table>
-          </div>
+          </ColumnWrapper>
         ))}
       </Wrapper>
     </div>
--- src/api.ts
+++ src/api.ts
@@ -4,6 +4,20 @@ const RANGES = "Оглавление";
 
 const api = `https://sheets.googleapis.com/v4/spreadsheets/${process.env.REACT_APP_SPREADSHEET_ID}/values:batchGet?ranges=${RANGES}&majorDimension=ROWS&key=${process.env.REACT_APP_GOOGLE_API_KEY}`;
 
+export interface Category {
+  slug: string;
+  name: string;
+  color: string;
+}
+
+const fakeCategories = [
+  { slug: "politics", name: "Политика", color: "#c6ce5b" },
+  { slug: "economy", name: "Экономика", color: "#5bce6f" },
+  { slug: "demography", name: "Демография", color: "#deaa46" },
+  { slug: "wars", name: "Войны", color: "#d46363" },
+  { slug: "industry", name: "Промышленность", color: "#8284d6" }
+];
+
 export interface Paragraph {
   id: string;
   chapter: string;
@@ -12,7 +26,11 @@ export interface Paragraph {
   timeRange: string;
   aberge: string;
   subjective: string;
+  categorySlug: string;
 }
+
+const getFakeCategorySlug = (s: string): string =>
+  fakeCategories[String(s).length % fakeCategories.length].slug;
 
 const getParagraphs = (data: any): Paragraph[] => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -28,7 +46,8 @@ const getParagraphs = (data: any): Paragraph[] => {
     description: values[3],
     timeRange: values[4],
     aberge: values[5],
-    subjective: values[6]
+    subjective: values[6],
+    categorySlug: getFakeCategorySlug(values[3])
   }));
   return paragraphs;
 };
@@ -38,5 +57,5 @@ export const fetchData = () =>
     .then(res => res.json())
     .then(data => {
       const paragraphs = getParagraphs(data);
-      return { paragraphs };
+      return { paragraphs, categories: fakeCategories };
     });
